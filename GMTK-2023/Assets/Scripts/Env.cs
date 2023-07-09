d579c1b0c5fa589f31d99d162037b0559b0f896d
Alexandre Santos <alexandressjr@gmail.com>
2023-07-09 13:48:02 -0300

    Adding Tile slots and logic

--- GMTK-2023/Assets/Scripts/Env.cs
+++ GMTK-2023/Assets/Scripts/Env.cs
@@ -12,7 +12,7 @@ public class Env : MonoBehaviour
 
     public enum Equips {Empty, WoodenSword, IronSword, GoldSword, WoodenShield, IronShield, GoldShield};
 
-    public int Coins = 0;
+    public int Coins = 100;
 
     public static float PrincessSpeed = 0.03f;
     public int PrincessLevel = 1;
@@ -29,54 +29,70 @@ public class Env : MonoBehaviour
     public Env.Paths pathExitSelection = Env.Paths.Empty;
     public Env.Slots itemSelection = Env.Slots.Empty;
 
+    public const int CoinsAmount = 100;
+    public const int SlimeDamage = 1;
+    public const int SwordDamageUpgrade = 2;
+    public const int ShieldDefenseUpgrade = 1;
+    public const int PathBuildCost = 5;
+    public const int SwordBuildCost = 20;
+    public const int ShieldBuildCost = 20;
+    public const int PotionBuildCost = 10;
+
     void Controls () {
         if (Input.GetKeyDown("1"))
         {
             pathEntrySelection = Env.Paths.South;
             pathExitSelection = Env.Paths.North;
             itemSelection = Env.Slots.Empty;
+            Coins -= PathBuildCost;
         } else if (Input.GetKeyDown("2"))
         {
             pathEntrySelection = Env.Paths.East;
             pathExitSelection = Env.Paths.West;
             itemSelection = Env.Slots.Empty;
+            Coins -= PathBuildCost;
         } else if (Input.GetKeyDown("3"))
         {
             pathEntrySelection = Env.Paths.South;
             pathExitSelection = Env.Paths.West;
             itemSelection = Env.Slots.Empty;
+            Coins -= PathBuildCost;
         } else if (Input.GetKeyDown("4"))
         {
             pathEntrySelection = Env.Paths.South;
             pathExitSelection = Env.Paths.East;
             itemSelection = Env.Slots.Empty;
+            Coins -= PathBuildCost;
         } else if (Input.GetKeyDown("5"))
         {
             pathEntrySelection = Env.Paths.East;
             pathExitSelection = Env.Paths.North;
             itemSelection = Env.Slots.Empty;
+            Coins -= PathBuildCost;
         } else if (Input.GetKeyDown("6"))
         {
             pathEntrySelection = Env.Paths.West;
             pathExitSelection = Env.Paths.North;
             itemSelection = Env.Slots.Empty;
+            Coins -= PathBuildCost;
         } else if (Input.GetKeyDown("q"))
         {
             itemSelection = Env.Slots.Sword;
             pathEntrySelection = Env.Paths.Empty;
             pathExitSelection = Env.Paths.Empty;
+            Coins -= SwordBuildCost;
         } else if (Input.GetKeyDown("w"))
         {
-            // itemSelection = Env.Slots.Sword;
             itemSelection = Env.Slots.Shield;
             pathEntrySelection = Env.Paths.Empty;
             pathExitSelection = Env.Paths.Empty;
+            Coins -= ShieldBuildCost;
         } else if (Input.GetKeyDown("e"))
         {
-            // itemSelection = Env.Slots.Shield;
             itemSelection = Env.Slots.Potion;
             pathEntrySelection = Env.Paths.Empty;
             pathExitSelection = Env.Paths.Empty;
+            Coins -= PotionBuildCost;
         }
     }
 
