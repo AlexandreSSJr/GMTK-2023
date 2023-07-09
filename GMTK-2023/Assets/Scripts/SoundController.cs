2020080d010021bf2a32d2f336e13c522c6bb021
Wendel Segovia Rodrigues <104607439+wenddelsr@users.noreply.github.com>
2023-07-09 12:55:59 -0300

    to apressado e nem sei qual nome colocar

---
+++ GMTK-2023/Assets/Scripts/SoundController.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SoundController : MonoBehaviour
+{
+    private bool soundState = true;
+    [SerializeField] private AudioSource backgroundMusic;
+
+    [SerializeField] private Sprite soundOn;
+    [SerializeField] private Sprite soundOff;
+
+    [SerializeField] private Image muteImage;
+    public void soundOnOff()
+    {
+        soundState = !soundState;
+        backgroundMusic.enabled = soundState;
+
+        if (soundState)
+        {
+            muteImage.sprite = soundOn;
+        }
+        else
+        {
+            muteImage.sprite = soundOff;
+        }
+    }
+}
