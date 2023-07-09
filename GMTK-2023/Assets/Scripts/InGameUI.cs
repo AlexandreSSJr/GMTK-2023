9a844c71527c7e1c0c7f8e3a8c014d44e5942c86
Wayne <waynegomes.wg@gmail.com>
2023-07-09 17:25:51 -0300

    IngameUI Responsitivity for at least 519px in height, Coin amount and attack linked with env.data

--- GMTK-2023/Assets/Scripts/InGameUI.cs
+++ GMTK-2023/Assets/Scripts/InGameUI.cs
@@ -7,6 +7,8 @@ public class InGameUI : MonoBehaviour
     public Env.Paths exit = Env.Paths.Empty;
     public Env.Slots slot = Env.Slots.Empty;
 
+    public int LastPrincessHealth;
+
     private void OnEnable()
     {
         // Getting the Root Element from the UIDocument
@@ -25,7 +27,7 @@ public class InGameUI : MonoBehaviour
 
         Button buttonReset = root.Q<Button>("ResetButton");
         Button buttonFastforward = root.Q<Button>("FastforwardButton");
-        
+
         buttonPathSouthNorth.clicked += () => OnPathSouthNorthClicked();
         buttonPathEastWest.clicked += () => OnPathEastWestClicked();
         buttonPathSouthWest.clicked += () => OnPathSouthWestClicked();
@@ -44,75 +46,124 @@ public class InGameUI : MonoBehaviour
     private void OnPathSouthNorthClicked()
     {
         Debug.Log("sn");
-            Env.Instance.pathEntrySelection = Env.Paths.South;
-            Env.Instance.pathExitSelection = Env.Paths.North;
-            Env.Instance.itemSelection = Env.Slots.Empty;
+        Env.Instance.pathEntrySelection = Env.Paths.South;
+        Env.Instance.pathExitSelection = Env.Paths.North;
+        Env.Instance.itemSelection = Env.Slots.Empty;
     }
+
     private void OnPathEastWestClicked()
     {
         Debug.Log("ew");
-            Env.Instance.pathEntrySelection = Env.Paths.East;
-            Env.Instance.pathExitSelection = Env.Paths.West;
-            Env.Instance.itemSelection = Env.Slots.Empty;
+        Env.Instance.pathEntrySelection = Env.Paths.East;
+        Env.Instance.pathExitSelection = Env.Paths.West;
+        Env.Instance.itemSelection = Env.Slots.Empty;
     }
+
     private void OnPathSouthWestClicked()
     {
         Debug.Log("sw");
-            Env.Instance.pathEntrySelection = Env.Paths.South;
-            Env.Instance.pathExitSelection = Env.Paths.West;
-            Env.Instance.itemSelection = Env.Slots.Empty;
+        Env.Instance.pathEntrySelection = Env.Paths.South;
+        Env.Instance.pathExitSelection = Env.Paths.West;
+        Env.Instance.itemSelection = Env.Slots.Empty;
     }
+
     private void OnPathSouthEastClicked()
     {
         Debug.Log("se");
-            Env.Instance.pathEntrySelection = Env.Paths.South;
-            Env.Instance.pathExitSelection = Env.Paths.East;
-            Env.Instance.itemSelection = Env.Slots.Empty;
+        Env.Instance.pathEntrySelection = Env.Paths.South;
+        Env.Instance.pathExitSelection = Env.Paths.East;
+        Env.Instance.itemSelection = Env.Slots.Empty;
     }
+
     private void OnPathEastNorthClicked()
     {
         Debug.Log("en");
-            Env.Instance.pathEntrySelection = Env.Paths.East;
-            Env.Instance.pathExitSelection = Env.Paths.North;
-            Env.Instance.itemSelection = Env.Slots.Empty;
+        Env.Instance.pathEntrySelection = Env.Paths.East;
+        Env.Instance.pathExitSelection = Env.Paths.North;
+        Env.Instance.itemSelection = Env.Slots.Empty;
     }
+
     private void OnPathWestNorthClicked()
     {
         Debug.Log("wn");
-            Env.Instance.pathEntrySelection = Env.Paths.West;
-            Env.Instance.pathExitSelection = Env.Paths.North;
-            Env.Instance.itemSelection = Env.Slots.Empty;
+        Env.Instance.pathEntrySelection = Env.Paths.West;
+        Env.Instance.pathExitSelection = Env.Paths.North;
+        Env.Instance.itemSelection = Env.Slots.Empty;
     }
+
     private void OnSwordClicked()
     {
         Debug.Log("sword");
-            Env.Instance.pathEntrySelection = Env.Paths.Empty;
-            Env.Instance.pathExitSelection = Env.Paths.Empty;
-            Env.Instance.itemSelection = Env.Slots.Sword;
+        Env.Instance.pathEntrySelection = Env.Paths.Empty;
+        Env.Instance.pathExitSelection = Env.Paths.Empty;
+        Env.Instance.itemSelection = Env.Slots.Sword;
     }
+
     private void OnShieldClicked()
     {
         Debug.Log("shield");
-            Env.Instance.pathEntrySelection = Env.Paths.Empty;
-            Env.Instance.pathExitSelection = Env.Paths.Empty;
-            Env.Instance.itemSelection = Env.Slots.Shield;
+        Env.Instance.pathEntrySelection = Env.Paths.Empty;
+        Env.Instance.pathExitSelection = Env.Paths.Empty;
+        Env.Instance.itemSelection = Env.Slots.Shield;
     }
+
     private void OnPotionClicked()
     {
         Debug.Log("potion");
-            Env.Instance.pathEntrySelection = Env.Paths.Empty;
-            Env.Instance.pathExitSelection = Env.Paths.Empty;
-            Env.Instance.itemSelection = Env.Slots.Potion;
+        Env.Instance.pathEntrySelection = Env.Paths.Empty;
+        Env.Instance.pathExitSelection = Env.Paths.Empty;
+        Env.Instance.itemSelection = Env.Slots.Potion;
     }
+
     private void OnResetClicked()
     {
         Debug.Log("reset");
         Env.Instance.PrincessSpeed = 0.03f;
         Application.LoadLevel(Application.loadedLevel);
     }
+
     private void OnFastforwardClicked()
     {
         Debug.Log("ff");
         Env.Instance.PrincessSpeed += 0.02f;
+    }
+
+    private void UpdateUIData()
+    {
+        // Getting the Root Element from the UIDocument
+        VisualElement root = GetComponent<UIDocument>().rootVisualElement;
+
+        Label labelCoins = root.Q<Label>("GoldValue");
+        Label labelAttackStatus = root.Q<Label>("AttackStats");
+
+        labelCoins.text = "" + Env.Instance.Coins;
+        labelAttackStatus.text = "" + Env.Instance.PrincessAttack;
+
+        if (Env.Instance.PrincessHealth != LastPrincessHealth) { }
+    }
+
+    private void UpdateHearts()
+    {
+        // // Getting the Root Element from the UIDocument
+        // VisualElement root = GetComponent<UIDocument>().rootVisualElement;
+
+        // VisualElement heartsContainer = root.Q<VisualElement>("HeartContainer");
+        // IMGUIContainer heart = root.Q<IMGUIContainer>("Heart");
+
+        // LastPrincessHealth = Env.Instance.PrincessHealth;
+
+        // IMGUIContainer heartCopy = heart.CloneTree();
+
+        // heartsContainer.Clear();
+
+        // for (int i = 0; i < LastPrincessHealth; i++)
+        // {
+        //     heartsContainer.Add(heartCopy);
+        // }
+    }
+
+    void Update()
+    {
+        UpdateUIData();
     }
 }
